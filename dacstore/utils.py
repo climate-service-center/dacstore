af326ffa5164b181d6a13ea3ea379fab7078f07c
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-07-30 17:21:16 +0200

    added utils

---
+++ dacstore/utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+from io import BytesIO
+
+
+survey_id = 1837044
+api_url = f"https://api.surveyhero.com/v1/surveys/{survey_id}/responses"
+
+
+def get_data(source=None, user=None, password=None):
+    """read csv from file or surveyhero api"""
+    if source is None:
+        params = {"format": "csv", "status": "completed"}
+        r = requests.get(api_url, params=params, auth=(user, password))
+        source = BytesIO(r.content)
+    df = pd.read_csv(source)
+    return df
