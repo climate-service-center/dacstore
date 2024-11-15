221933254711674b97ddcc1258e7fbd67c513b73
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-11-15 14:37:26 +0100

    update model notebook

--- dacstore/model.py
+++ dacstore/model.py
@@ -17,6 +17,18 @@ def multicollinearity(X):
     return vif_data
 
 
+def cronbach_alpha(df):
+    # Number of items (questions)
+    N = df.shape[1]
+    # Variance of each item
+    item_variances = df.var(axis=0, ddof=1)
+    # Total variance of the sum of all items
+    total_variance = df.sum(axis=1).var(ddof=1)
+    # Cronbach's alpha calculation
+    alpha = (N / (N - 1)) * (1 - (item_variances.sum() / total_variance))
+    return alpha
+
+
 def create_model(y, X):
     """Create a model"""
     X = sm.add_constant(X)
